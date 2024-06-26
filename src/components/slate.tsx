fb484da43c68b2abd25497eec0ba7f7c03dde252
Dan Kral <dan@bluestonelogic.com>
2024-06-26 14:46:32 -0400

    create slate.tsx example

---
+++ src/components/slate.tsx
@@ -0,0 +1,545 @@
+import React, {
+  useMemo,
+  useCallback,
+  useRef,
+  useEffect,
+  useState,
+  type ReactNode,
+} from 'react';
+import { Editor, Transforms, Range, createEditor, Descendant } from 'slate';
+import { withHistory } from 'slate-history';
+import {
+  Slate,
+  Editable,
+  ReactEditor,
+  withReact,
+  useSelected,
+  useFocused,
+} from 'slate-react';
+
+import { BaseEditor, BaseRange } from 'slate';
+import { HistoryEditor } from 'slate-history';
+
+import { createPortal } from 'react-dom';
+
+const MentionExample = () => {
+  const ref = useRef<HTMLDivElement | null>();
+  const [target, setTarget] = useState<Range | undefined>();
+  const [index, setIndex] = useState(0);
+  const [search, setSearch] = useState('');
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(
+    () => withMentions(withReact(withHistory(createEditor()))),
+    []
+  );
+
+  const chars = CHARACTERS.filter((c) =>
+    c.toLowerCase().startsWith(search.toLowerCase())
+  ).slice(0, 10);
+
+  const onKeyDown = useCallback(
+    (event) => {
+      if (target && chars.length > 0) {
+        switch (event.key) {
+          case 'ArrowDown':
+            event.preventDefault();
+            const prevIndex = index >= chars.length - 1 ? 0 : index + 1;
+            setIndex(prevIndex);
+            break;
+          case 'ArrowUp':
+            event.preventDefault();
+            const nextIndex = index <= 0 ? chars.length - 1 : index - 1;
+            setIndex(nextIndex);
+            break;
+          case 'Tab':
+          case 'Enter':
+            event.preventDefault();
+            Transforms.select(editor, target);
+            insertMention(editor, chars[index]);
+            setTarget(null);
+            break;
+          case 'Escape':
+            event.preventDefault();
+            setTarget(null);
+            break;
+        }
+      }
+    },
+    [chars, editor, index, target]
+  );
+
+  useEffect(() => {
+    if (target && chars.length > 0) {
+      const el = ref.current;
+      const domRange = ReactEditor.toDOMRange(editor, target);
+      const rect = domRange.getBoundingClientRect();
+      el.style.top = `${rect.top + window.pageYOffset + 24}px`;
+      el.style.left = `${rect.left + window.pageXOffset}px`;
+    }
+  }, [chars.length, editor, index, search, target]);
+
+  return (
+    <Slate
+      editor={editor}
+      initialValue={initialValue}
+      onChange={() => {
+        const { selection } = editor;
+
+        if (selection && Range.isCollapsed(selection)) {
+          const [start] = Range.edges(selection);
+          const wordBefore = Editor.before(editor, start, { unit: 'word' });
+          const before = wordBefore && Editor.before(editor, wordBefore);
+          const beforeRange = before && Editor.range(editor, before, start);
+          const beforeText = beforeRange && Editor.string(editor, beforeRange);
+          const beforeMatch = beforeText && beforeText.match(/^\s(\w+)$/);
+          const after = Editor.after(editor, start);
+          const afterRange = Editor.range(editor, start, after);
+          const afterText = Editor.string(editor, afterRange);
+          const afterMatch = afterText.match(/^(\s|$)/);
+
+          if (beforeMatch && afterMatch) {
+            setTarget(beforeRange);
+            setSearch(beforeMatch[1]);
+            setIndex(0);
+            return;
+          }
+        }
+
+        setTarget(null);
+      }}
+    >
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={onKeyDown}
+        placeholder="Enter some text..."
+      />
+      {target && chars.length > 0 && (
+        <Portal>
+          <div
+            ref={ref}
+            style={{
+              top: '-9999px',
+              left: '-9999px',
+              position: 'absolute',
+              zIndex: 1,
+              padding: '3px',
+              background: 'white',
+              borderRadius: '4px',
+              boxShadow: '0 1px 5px rgba(0,0,0,.2)',
+            }}
+            data-cy="mentions-portal"
+          >
+            {chars.map((char, i) => (
+              <div
+                key={char}
+                onClick={() => {
+                  Transforms.select(editor, target);
+                  insertMention(editor, char);
+                  setTarget(null);
+                }}
+                style={{
+                  padding: '1px 3px',
+                  borderRadius: '3px',
+                  background: i === index ? '#B4D5FF' : 'transparent',
+                }}
+              >
+                {char}
+              </div>
+            ))}
+          </div>
+        </Portal>
+      )}
+    </Slate>
+  );
+};
+
+const withMentions = (editor) => {
+  const { isInline, isVoid, markableVoid } = editor;
+
+  editor.isInline = (element) => {
+    return element.type === 'mention' ? true : isInline(element);
+  };
+
+  editor.isVoid = (element) => {
+    return element.type === 'mention' ? true : isVoid(element);
+  };
+
+  editor.markableVoid = (element) => {
+    return element.type === 'mention' || markableVoid(element);
+  };
+
+  return editor;
+};
+
+const insertMention = (editor, character) => {
+  const mention: MentionElement = {
+    type: 'mention',
+    character,
+    children: [{ text: '' }],
+  };
+  Transforms.insertNodes(editor, mention);
+  Transforms.move(editor);
+};
+
+// Borrow Leaf renderer from the Rich Text example.
+// In a real project you would get this via `withRichText(editor)` or similar.
+const Leaf = ({ attributes, children, leaf }) => {
+  if (leaf.bold) {
+    children = <strong>{children}</strong>;
+  }
+
+  if (leaf.code) {
+    children = <code>{children}</code>;
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>;
+  }
+
+  if (leaf.underline) {
+    children = <u>{children}</u>;
+  }
+
+  return <span {...attributes}>{children}</span>;
+};
+
+const Element = (props) => {
+  const { attributes, children, element } = props;
+  switch (element.type) {
+    case 'mention':
+      return <Mention {...props} />;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+
+const Mention = ({ attributes, children, element }) => {
+  const selected = useSelected();
+  const focused = useFocused();
+  const style: React.CSSProperties = {
+    padding: '3px 3px 2px',
+    margin: '0 1px',
+    verticalAlign: 'baseline',
+    display: 'inline-block',
+    borderRadius: '4px',
+    backgroundColor: '#eee',
+    fontSize: '0.9em',
+    boxShadow: selected && focused ? '0 0 0 2px #B4D5FF' : 'none',
+  };
+  // See if our empty text child has any styling marks applied and apply those
+  if (element.children[0].bold) {
+    style.fontWeight = 'bold';
+  }
+  if (element.children[0].italic) {
+    style.fontStyle = 'italic';
+  }
+  return (
+    <span
+      {...attributes}
+      contentEditable={false}
+      data-cy={`mention-${element.character.replace(' ', '-')}`}
+      style={style}
+    >
+      {element.character}
+      {children}
+    </span>
+  );
+};
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: 'This example shows how you might implement a simple ',
+      },
+    ],
+  },
+];
+
+const CHARACTERS = [
+  'abort',
+  'action',
+  'add',
+  'after',
+  'all',
+  'alter',
+  'always',
+  'analyze',
+  'and',
+  'as',
+  'asc',
+  'attach',
+  'autoincrement',
+  'before',
+  'begin',
+  'between',
+  'by',
+  'cascade',
+  'case',
+  'cast',
+  'check',
+  'collate',
+  'column',
+  'commit',
+  'conflict',
+  'constraint',
+  'create',
+  'cross',
+  'current',
+  'current_date',
+  'current_time',
+  'current_timestamp',
+  'database',
+  'default',
+  'deferrable',
+  'deferred',
+  'delete',
+  'desc',
+  'detach',
+  'distinct',
+  'do',
+  'drop',
+  'each',
+  'else',
+  'end',
+  'escape',
+  'except',
+  'exclude',
+  'exclusive',
+  'exists',
+  'explain',
+  'fail',
+  'filter',
+  'first',
+  'following',
+  'for',
+  'foreign',
+  'from',
+  'full',
+  'generated',
+  'glob',
+  'group',
+  'groups',
+  'having',
+  'if',
+  'ignore',
+  'immediate',
+  'in',
+  'index',
+  'indexed',
+  'initially',
+  'inner',
+  'insert',
+  'instead',
+  'intersect',
+  'into',
+  'is',
+  'isnull',
+  'join',
+  'key',
+  'last',
+  'left',
+  'like',
+  'limit',
+  'match',
+  'materialized',
+  'natural',
+  'no',
+  'not',
+  'nothing',
+  'notnull',
+  'null',
+  'nulls',
+  'of',
+  'offset',
+  'on',
+  'or',
+  'order',
+  'others',
+  'outer',
+  'over',
+  'partition',
+  'plan',
+  'pragma',
+  'preceding',
+  'primary',
+  'query',
+  'raise',
+  'range',
+  'recursive',
+  'references',
+  'regexp',
+  'reindex',
+  'release',
+  'rename',
+  'replace',
+  'restrict',
+  'returning',
+  'right',
+  'rollback',
+  'row',
+  'rows',
+  'savepoint',
+  'select',
+  'set',
+  'table',
+  'temp',
+  'temporary',
+  'then',
+  'ties',
+  'to',
+  'transaction',
+  'trigger',
+  'unbounded',
+  'union',
+  'unique',
+  'update',
+  'using',
+  'vacuum',
+  'values',
+  'view',
+  'virtual',
+  'when',
+  'where',
+  'window',
+  'with',
+  'without',
+];
+
+export default MentionExample;
+
+export const Portal = ({ children }: { children?: ReactNode }) => {
+  return typeof document === 'object'
+    ? createPortal(children, document.body)
+    : null;
+};
+
+export type BlockQuoteElement = {
+  type: 'block-quote';
+  align?: string;
+  children: Descendant[];
+};
+
+export type BulletedListElement = {
+  type: 'bulleted-list';
+  align?: string;
+  children: Descendant[];
+};
+
+export type CheckListItemElement = {
+  type: 'check-list-item';
+  checked: boolean;
+  children: Descendant[];
+};
+
+export type EditableVoidElement = {
+  type: 'editable-void';
+  children: EmptyText[];
+};
+
+export type HeadingElement = {
+  type: 'heading';
+  align?: string;
+  children: Descendant[];
+};
+
+export type HeadingTwoElement = {
+  type: 'heading-two';
+  align?: string;
+  children: Descendant[];
+};
+
+export type ImageElement = {
+  type: 'image';
+  url: string;
+  children: EmptyText[];
+};
+
+export type LinkElement = { type: 'link'; url: string; children: Descendant[] };
+
+export type ButtonElement = { type: 'button'; children: Descendant[] };
+
+export type BadgeElement = { type: 'badge'; children: Descendant[] };
+
+export type ListItemElement = { type: 'list-item'; children: Descendant[] };
+
+export type MentionElement = {
+  type: 'mention';
+  character: string;
+  children: CustomText[];
+};
+
+export type ParagraphElement = {
+  type: 'paragraph';
+  align?: string;
+  children: Descendant[];
+};
+
+export type TitleElement = { type: 'title'; children: Descendant[] };
+
+export type VideoElement = {
+  type: 'video';
+  url: string;
+  children: EmptyText[];
+};
+
+export type CodeBlockElement = {
+  type: 'code-block';
+  language: string;
+  children: Descendant[];
+};
+
+export type CodeLineElement = {
+  type: 'code-line';
+  children: Descendant[];
+};
+
+type CustomElement =
+  | BlockQuoteElement
+  | BulletedListElement
+  | CheckListItemElement
+  | EditableVoidElement
+  | HeadingElement
+  | HeadingTwoElement
+  | ImageElement
+  | LinkElement
+  | ButtonElement
+  | BadgeElement
+  | ListItemElement
+  | MentionElement
+  | ParagraphElement
+  | TitleElement
+  | VideoElement
+  | CodeBlockElement
+  | CodeLineElement;
+
+export type CustomText = {
+  bold?: boolean;
+  italic?: boolean;
+  code?: boolean;
+  text: string;
+};
+
+export type EmptyText = {
+  text: string;
+};
+
+export type CustomEditor = BaseEditor &
+  ReactEditor &
+  HistoryEditor & {
+    nodeToDecorations?: Map<Element, Range[]>;
+  };
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: CustomEditor;
+    Element: CustomElement;
+    Text: CustomText | EmptyText;
+    Range: BaseRange & {
+      [key: string]: unknown;
+    };
+  }
+}
