365a9e2e1da7cdce2503739f7719f121793d486e
Dan Kral <dankral01@gmail.com>
2024-08-05 13:34:00 -0400

    add fun ascii art

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -40,27 +40,59 @@ function App() {
     // []
     [
       {
-        time: new Date("2024-06-26T17:18:32.163Z"),
-        text: "create table users (id integer primary key, name text);",
-        id: "01J1ARWQPD1A0S7CHEXFT21CMM",
+        time: new Date(),
+        // this is all fucked up for escaping characters
+        text: `
+
+        +-----------------------------------------------+
+        |                                               |
+        |                                               |
+        |                 ___        __                 |
+        |                /\\_ \\    __/\\ \\__              |
+        |    ____     __ \\//\\ \\  /\\_\\ \\ ,_\\    __       |
+        |   /',__\\  /'__\`\\ \\ \\ \\ \\/\\ \\ \\ \\/  /'__\`\\     |
+        |  /\\__, \`\\/\\ \\L\\ \\ \\_\\ \\_\\ \\ \\ \\ \\_/\\  __/     |
+        |  \\/\\____/\\ \\___, \\/\\____\\\\ \\_\\ \\__\\ \\____\\    |
+        |   \\/___/  \\/___/\\ \\/____/ \\/_/\\/__/\\/____/    |
+        |                \\ \\_\\                          |
+        |                 \\/_/                          |
+        |         __                __                  |
+        |        /\\ \\__            /\\ \\  __             |
+        |    ____\\ \\ ,_\\  __  __   \\_\\ \\/\\_\\    ___     |
+        |   /',__\\\\ \\ \\/ /\\ \\/\\ \\  /'_\` \\/\\ \\  / __\`\\   |
+        |  /\\__, \`\\\\ \\ \\_\\ \\ \\_\\ \\/\\ \\L\\ \\ \\ \\/\\ \\L\\ \\  |
+        |  \\/\\____/ \\ \\__\\\\ \\____/\\ \\___,_\\ \\_\\ \\____/  |
+        |   \\/___/   \\/__/ \\/___/  \\/__,_ /\\/_/\\/___/   |
+        |                                               |
+        |                                               |
+        +-----------------------------------------------+
+
+        `,
+        id: "01J1ARWQPD1A0S7CHEXFT21CMB",
         output: [],
       },
-      {
-        time: new Date("2024-06-26T17:18:53.056Z"),
-        text: "insert into users (name) values ('dan');",
-        id: "01J1ARXC3VXH77C6V7ZWXS5QS7",
-        output: [],
-      },
-      {
-        time: new Date("2024-06-26T17:19:00.166Z"),
-        text: "select * from users;",
-        id: "01J1ARXK23N0XA5FN8VN8RRQ07",
-        output: [
-          {
-            name: "dan",
-          },
-        ],
-      },
+      // {
+      //   time: new Date("2024-06-26T17:18:32.163Z"),
+      //   text: "create table users (id integer primary key, name text);",
+      //   id: "01J1ARWQPD1A0S7CHEXFT21CMM",
+      //   output: [],
+      // },
+      // {
+      //   time: new Date("2024-06-26T17:18:53.056Z"),
+      //   text: "insert into users (name) values ('dan');",
+      //   id: "01J1ARXC3VXH77C6V7ZWXS5QS7",
+      //   output: [],
+      // },
+      // {
+      //   time: new Date("2024-06-26T17:19:00.166Z"),
+      //   text: "select * from users;",
+      //   id: "01J1ARXK23N0XA5FN8VN8RRQ07",
+      //   output: [
+      //     {
+      //       name: "dan",
+      //     },
+      //   ],
+      // },
     ]
   );
   const [currentText, setCurrentText] = useState<string>("");
